d253fd227d31e8adfa9a04d707e1c8263926b1fa
oooo1111880 <oooo1111880@gmail.com>
2023-04-24 11:53:49 +0800

    D0947762 login&signup UI and code upload

---
+++ app/src/main/java/com/example/a417d0947762/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.a417d0947762;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView etUsername;
+    private TextView etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etUsername = findViewById(R.id.et_userNameLogin);
+        etPassword = findViewById(R.id.et_passwordLogin);
+        Button login = findViewById(R.id.bt_login);
+        Button signup = findViewById(R.id.bt_signup);
+
+        View.OnClickListener listener = view -> {
+            if(view.getId() == R.id.bt_login) {
+                SharedPreferences sharedPreferences = getSharedPreferences("Account", MODE_PRIVATE);
+                String userName = sharedPreferences.getString("username", "");
+                String password = sharedPreferences.getString("password", "");
+
+                if(userName.equals(etUsername.getText().toString()) && password.equals(etPassword.getText().toString()) && !etUsername.getText().toString().equals("") && !etPassword.getText().toString().equals("")) {
+                    Intent intent = new Intent(MainActivity.this, ProductListActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "帳號或密碼錯誤", Toast.LENGTH_SHORT).show();
+                }
+
+
+            } else if(view.getId() == R.id.bt_signup) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        };
+        login.setOnClickListener(listener);
+        signup.setOnClickListener(listener);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
